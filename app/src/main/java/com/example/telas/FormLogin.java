04e9dd640b638c5978c914fdd858651541d645e0
Larssaa <larissajenyferachatatola@gmail.com>
2023-05-25 15:41:31 -0300

    Atualização no dia 25/05

--- app/src/main/java/com/example/telas/FormLogin.java
+++ app/src/main/java/com/example/telas/FormLogin.java
@@ -2,13 +2,32 @@ package com.example.telas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class FormLogin extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_login);
+
+        getSupportActionBar().hide();
+        InciarComponentes();
+        text_tela_cadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(FormLogin.this,FormLogin.this.getClass());
+                startActivity(intent);
+            }
+        });
+    }
+    private void InciarComponentes(){
+        text_tela_cadastro = findViewById(R.id.text_tela_cadastrar);
+
     }
 }
